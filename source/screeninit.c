a4ea5654171095612dd90e9c1c9080926d515e06
OperationNT414C <OperationNT414C@users.noreply.github.com>
2016-08-18 23:18:30 +0200

    Update screeninit.c

--- source/screeninit.c
+++ source/screeninit.c
@@ -44,25 +44,52 @@ void deinitScreens(void)
     }
 }
 
-void initScreens(void)
+const u32 brightness[BRIGHTNESS_COUNT] = {0x5F, 0x4C, 0x39, 0x26};
+
+void initScreens(u8 brightnessLevel)
 {
     if(PDN_GPU_CNT == 1)
     {
         u32 *const screenInitAddress = (u32 *)0x24FFFC00;
-
         memcpy(screenInitAddress, screeninit, screeninit_size);
 
         //Write brightness level for the stub to pick up
-        //screenInitAddress[2] = MULTICONFIG(0);
-
+        screenInitAddress[2] = brightnessLevel%BRIGHTNESS_COUNT;
+        
         *arm11Entry = (u32)screenInitAddress;
         while(*arm11Entry);
 
         //Turn on backlight
         i2cWriteRegister(I2C_DEV_MCU, 0x22, 0x2A);
     }
-
-    //clearScreens();
 }
+
+/*void updateBrightness(u8 brightnessLevel)
+{
+    static int brightnessValue;
+    brightnessValue = brightness[brightnessLevel%BRIGHTNESS_COUNT];
+    
+    void __attribute__((naked)) ARM11(void)
+    {
+        //Disable interrupts
+        __asm(".word 0xF10C01C0");      
+        
+        //Clear ARM11 entry offset
+        *arm11Entry = 0;
+
+        //Change brightness
+        *(vu32 *)0x10202240 = brightnessValue;
+        *(vu32 *)0x10202A40 = brightnessValue;
+
+        //Wait for the entry to be set
+        while(!*arm11Entry);
+
+        //Jump to it
+        ((void (*)())*arm11Entry)();
+    }
+
+    *arm11Entry = (u32)ARM11;
+    while(*arm11Entry);
+}*/
 
 #endif
