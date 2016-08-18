01ad83eec9cbe486df72bda992793253d3860827
OperationNT414C <OperationNT414C@users.noreply.github.com>
2016-08-18 23:18:53 +0200

    Update screeninit.h

--- source/screeninit.h
+++ source/screeninit.h
@@ -11,9 +11,11 @@
 
 #include "arm9/source/common.h"
 
+#define BRIGHTNESS_COUNT   0x4
 #define PDN_GPU_CNT        (*(vu8 *)0x10141200)
 
 void deinitScreens(void);
-void initScreens(void);
+void initScreens(u8 brightnessLevel);
+//void updateBrightness(u8 brightnessLevel);
 
 #endif
