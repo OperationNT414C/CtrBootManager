683a4d2b3c59652c557ef53270b0e19b54212611
OperationNT414C <OperationNT414C@users.noreply.github.com>
2017-06-07 23:49:22 +0200

    Update firm.c

--- source/arm9/source/firm.c
+++ source/arm9/source/firm.c
@@ -1,56 +1,60 @@
 #include "firm.h"
 
 #include "crypto.h"
+#include "../../../build/chainloader.h"
 
 static __attribute__((noinline)) bool overlaps(u32 as, u32 ae, u32 bs, u32 be)
 {
-    if (as <= bs && bs <= ae)
+    if(as <= bs && bs <= ae)
         return true;
-    else if (bs <= as && as <= be)
+    if(bs <= as && as <= be)
         return true;
     return false;
 }
 
-bool checkFirmPayload(Firm* firm)
+static __attribute__((noinline)) bool inRange(u32 as, u32 ae, u32 bs, u32 be)
 {
-    if(memcmp(firm->magic, "FIRM", 4) != 0)
+   if(as >= bs && ae <= be)
+        return true;
+   return false;
+}
+
+bool checkFirmPayload(Firm* firm, u32 payloadSize)
+{
+    if(memcmp(firm->magic, "FIRM", 4) != 0 || firm->arm9Entry == NULL) //Allow for the ARM11 entrypoint to be zero in which case nothing is done on the ARM11 side
         return false;
 
-    if(firm->arm9Entry == NULL)  //allow for the arm11 entrypoint to be zero in which case nothing is done on the arm11 side
-        return false;
+    bool arm9EpFound = false,
+         arm11EpFound = false;
 
     u32 size = 0x200;
     for(u32 i = 0; i < 4; i++)
         size += firm->section[i].size;
 
-    bool arm9EpFound = false, arm11EpFound = false;
+    if(size != payloadSize) return false;
+
     for(u32 i = 0; i < 4; i++)
     {
-        __attribute__((aligned(4))) u8 hash[0x20];
-
         FirmSection *section = &firm->section[i];
 
-        // allow empty sections
-        if (section->size == 0)
+        //Allow empty sections
+        if(section->size == 0)
             continue;
 
-        if(section->offset < 0x200)
+        if((section->offset < 0x200) ||
+           (section->address + section->size < section->address) || //Overflow check
+           ((u32)section->address & 3) || (section->offset & 0x1FF) || (section->size & 0x1FF) || //Alignment check
+           (overlaps((u32)section->address, (u32)section->address + section->size, (u32)firm, (u32)firm + size)) ||
+           ((!inRange((u32)section->address, (u32)section->address + section->size, 0x08000000, 0x08000000 + 0x00100000)) &&
+            (!inRange((u32)section->address, (u32)section->address + section->size, 0x18000000, 0x18000000 + 0x00600000)) &&
+            (!inRange((u32)section->address, (u32)section->address + section->size, 0x1FF00000, 0x1FFFFC00)) &&
+            (!inRange((u32)section->address, (u32)section->address + section->size, 0x20000000, 0x20000000 + 0x8000000))))
             return false;
 
-        if(section->address + section->size < section->address) //overflow check
-            return false;
-
-        if(((u32)section->address & 3) || (section->offset & 0x1FF) || (section->size & 0x1FF)) //alignment check
-            return false;
-
-        if(overlaps((u32)section->address, (u32)section->address + section->size, 0x27FFE000, 0x28000000))
-            return false;
-        else if(overlaps((u32)section->address, (u32)section->address + section->size, 0x27FFE000 - 0x1000, 0x27FFE000))
-            return false;
-        else if(overlaps((u32)section->address, (u32)section->address + section->size, (u32)firm, (u32)firm + size))
-            return false;
+        __attribute__((aligned(4))) u8 hash[0x20];
 
         sha(hash, (u8 *)firm + section->offset, section->size, SHA_256_MODE);
+
         if(memcmp(hash, section->hash, 0x20) != 0)
             return false;
 
@@ -62,4 +66,17 @@ bool checkFirmPayload(Firm* firm)
     }
 
     return arm9EpFound && (firm->arm11Entry == NULL || arm11EpFound);
+}
+
+void launchFirm(int argc, char **argv)
+{
+    u32 *chainloaderAddress = (u32 *)0x01FF9000;
+
+    // Should not be needed: no ARM11 process in background
+    //prepareArm11ForFirmlaunch();
+
+    memcpy(chainloaderAddress, chainloader, chainloader_size);
+
+    // No need to flush caches here, the chainloader is in ITCM
+    ((void (*)(int, char **, u32))chainloaderAddress)(argc, argv, 0x0000BEEF);
 }
