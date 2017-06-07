4517e85cc819223c1b6493fcc28438e6b7b9cb24
OperationNT414C <OperationNT414C@users.noreply.github.com>
2017-06-07 23:37:01 +0200

    Update and rename source/screeninit.h to source/arm9/source/screen.h

--- source/arm9/source/screen.h
+++ source/arm9/source/screen.h
@@ -7,15 +7,19 @@
 
 #pragma once
 
-#ifdef ARM9
-
-#include "arm9/source/common.h"
+#include "common.h"
 
 #define BRIGHTNESS_COUNT   0x4
 #define PDN_GPU_CNT        (*(vu8 *)0x10141200)
 
-void deinitScreens(void);
+struct fb {
+     u8 *top_left;
+     u8 *top_right;
+     u8 *bottom;
+}  __attribute__((packed));
+
+extern struct fb fbs[2];
+
+//void deinitScreens(void);
 void initScreens(u8 brightnessLevel);
 //void updateBrightness(u8 brightnessLevel);
-
-#endif
