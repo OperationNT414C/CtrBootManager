1bf9d565cc2e6a01b02802010ff787bfa4d5e7e7
OperationNT414C <OperationNT414C@users.noreply.github.com>
2017-06-07 23:44:13 +0200

    Update main.c

--- source/main.c
+++ source/main.c
@@ -8,15 +8,16 @@
 
 #ifdef ARM9
 
+#include "memory.h"
 #include "arm9/source/common.h"
 #include "arm9/source/hid.h"
 #include "arm9/source/fatfs/ff.h"
-#include "screeninit.h"
+#include "arm9/source/screen.h"
 
 #else
+
 #include <3ds.h>
 #include "hb_menu/netloader.h"
-
 
 extern char boot_app[512];
 extern bool boot_app_enabled;
@@ -56,7 +57,11 @@ void __appExit() {
 }
 #endif
 
-int main(int argc, char **argv) {
+#ifdef ARM9
+int startCtrBootManager(int argc, char **argv, u32 magicWord) {
+#else
+int main() {
+#endif
 
 #ifdef ARM9
     *(u32 *) 0x10000020 = 0;
@@ -64,13 +69,13 @@ int main(int argc, char **argv) {
 
     // Code inspired from Luma3DS
     u16 launchedPath[4];
-    if (1 == argc)
+    if ((magicWord & 0xFFFF) == 0xBEEF && argc >= 1)
     {
         u32 i;
         for(i = 0; i < 4 && argv[0][i] != 0; i++)
             launchedPath[i] = argv[0][i];
     }
-    else if (2 == argc)
+    else if (magicWord == 0xBABE && argc == 2)
     {
         u32 i;
         u16 *p = (u16 *)argv[0];
@@ -80,6 +85,40 @@ int main(int argc, char **argv) {
     else
         launchedPath[0] = 0;
 
+    // Framebuffers management
+    if(argc >= 2)
+    {
+        u8 **fb = (u8 **)(void *)argv[1];
+        fbs[0].top_left = fb[0];
+        fbs[0].top_right = fb[1];
+        fbs[0].bottom = fb[2];
+
+        if (argc >= 4)
+        {
+            fbs[1].top_left = fb[3];
+            fbs[1].top_right = fb[4];
+            fbs[1].bottom = fb[5];
+        }
+        else
+        {
+            fbs[1].top_left = fbs[0].top_left;
+            fbs[1].top_right = fbs[0].top_right;
+            fbs[1].bottom = fbs[0].bottom;
+        }
+        
+        *PTR_TOP_SCREEN = fbs[0].top_left;
+        *PTR_BOT_SCREEN = fbs[0].bottom;
+    }
+    else
+    {
+        fbs[0].top_left = (u8 *)0x18300000;
+        fbs[1].top_left = (u8 *)0x18300000;
+        fbs[0].top_right = (u8 *)0x18300000;
+        fbs[1].top_right = (u8 *)0x18300000;
+        fbs[0].bottom = (u8 *)0x18346500;
+        fbs[1].bottom = (u8 *)0x18346500;
+    }
+    
     bool loadedFromSD = (memcmp(launchedPath, "n\0a\0n\0d\0", 8) != 0);
     initFileSystems(loadedFromSD);
 #else
@@ -91,9 +130,9 @@ int main(int argc, char **argv) {
     osSetSpeedupEnable(true);
 
     // offset potential issues caused by homebrew that just ran (from hb_menu)
-    aptOpenSession();
+    //aptOpenSession();
     APT_SetAppCpuTimeLimit(0);
-    aptCloseSession();
+    //aptCloseSession();
 
     if (!boot_app_enabled) { // fix SOC_Initialize
 #endif
