3d45a15cc32819335d3a5d3f999060e0e14b5ac4
OperationNT414C <OperationNT414C@users.noreply.github.com>
2017-06-07 23:44:40 +0200

    Update memory.h

--- source/memory.h
+++ source/memory.h
@@ -5,8 +5,8 @@
 #ifndef CTRBOOTMANAGER_MEMORY_H
 #define CTRBOOTMANAGER_MEMORY_H
 
-#define PTR_TOP_SCREEN          (u8*)(*(u32*)0x23FFFE00)
-#define PTR_BOT_SCREEN          (u8*)(*(u32*)0x23FFFE08)
+#define PTR_TOP_SCREEN          (void**)(0x23FFFE00)
+#define PTR_BOT_SCREEN          (void**)(0x23FFFE08)
 #define PTR_TOP_SCREEN_BUF      (u8*)(0x25000000)
 #define PTR_BOT_SCREEN_BUF      (u8*)(0x25046500)
 #define PTR_TOP_BG              (u8*)(0x2507E900)
@@ -27,6 +27,6 @@
 #define PTR_PAYLOAD_STAGE2      0x08006000
 
 #define PTR_PAYLOAD_FIRM        0x27FFE000
-#define PTR_PAYLOAD_FIRM_DATA   0x24000000
+#define PTR_PAYLOAD_FIRM_DATA   0x20001000
 
 #endif //CTRBOOTMANAGER_MEMORY_H
